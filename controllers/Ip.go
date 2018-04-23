c7a21a444ddf5a437def38c8afd1813034653138
zhufeixiang <2970131851@qq.com>
2018-04-23 16:03:06 +0800

    重构获取ip代码改用协程【完成】

--- controllers/Ip.go
+++ controllers/Ip.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 	"github.com/garyburd/redigo/redis"
 	"strings"
+	"github.com/gamexg/proxyclient"
+	"io"
+	"time"
+	"io/ioutil"
 )
 
 type IpController struct {
@@ -26,17 +30,20 @@ func (this IpController) Get() {
 
 
 	var ips []string
-	for _, v := range values {
+	chs := make([]chan bool,len(values))
+	for k, v := range values {
 		ip2port := string(v.([]byte))
 		res := strings.Contains(ip2port,":")
 		fmt.Println(res)
 		if res != true {
 			continue
 		}
+		chs[k] = make(chan bool)
 
-		_,err := this.checkProxy(ip2port,"http")
 
-		if err != nil {//删除不可用ip
+	   go this.checkProxy(ip2port,"http",chs[k])
+
+		if <-chs[k] == false {//删除不可用ip
 			c.Do("HDEL", "useful_proxy",ip2port)
 			continue
 		}
@@ -55,6 +62,67 @@ func (this IpController) Get() {
 	}
 
 	this.OkReturn("success",MSG_OK,ips)
+
+}
+
+func(self *IpController) checkProxy(ip2port string,proxyType string,chs chan bool) {
+
+	var address string
+	switch proxyType {
+	case "http":
+		address = "http://"+ip2port
+		break
+	case "https":
+		address = "https://"+ip2port
+		break
+	case "socks5":
+		address = "socks5://"+ip2port
+		break
+	default:
+		address = "http://"+ip2port
+		break
+
+
+	}
+
+	p,err := proxyclient.NewProxyClient(address)
+	if err != nil {
+		panic(err)
+	}
+
+	c, err := p.Dial("tcp", "www.baidu.com:80")
+
+	if err != nil {
+		fmt.Println(err.Error())
+		chs <- false
+	}
+
+
+	io.WriteString(c, "GET / HTTP/1.0\r\nHOST:baidu.com\r\n\r\n")
+	//计算响应时长
+	t1 := time.Now() // get current time
+	b, err := ioutil.ReadAll(c)
+
+	if err != nil {
+		panic(err)
+		chs <- false
+	}
+
+	fmt.Println(string(b))
+
+	res := strings.Contains(string(b),"无效用户")
+	if res == true {
+		chs <- false
+	}
+
+	elapsed := time.Since(t1)
+
+	data := make(map[string]interface{})
+
+	fmt.Println("App elapsed: ", elapsed)
+	data["spend"] = elapsed.String()
+
+	chs <- true
 
 }
 
